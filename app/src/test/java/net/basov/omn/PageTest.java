3b08b3885c19c872c55c5951679a5431426157e9
Mikhail Basov <mikhail[at]basov[dot]net>
2017-10-27 19:20:21 +0300

    Add JUnit test for Page class

---
+++ app/src/test/java/net/basov/omn/PageTest.java
@@ -0,0 +1,51 @@
+package net.basov.omn;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by mvb on 10/27/17.
+ */
+public class PageTest {
+
+    private Page expectedPage;
+    private Page realPage;
+
+    public PageTest() {
+        this.realPage = new Page("/some/Page#refer");
+    }
+
+    @Test
+    public void testGetInPageReference() throws Exception {
+        Assert.assertEquals(
+                "#refer",
+                this.realPage.getInPageReference()
+        );
+    }
+
+    @Test
+    public void testGetPageName() throws Exception {
+        Assert.assertEquals(
+                "/some/Page",
+                this.realPage.getPageName()
+        );
+    }
+
+    @Test
+    public void testSetMdContent() throws Exception {
+        String mdHeader =
+                "Title: title\n"
+                + "Date: 2017-10-10 10:10:10\n";
+        String mdContent =
+                "\n"
+                + "Note text.\n";
+        this.realPage.setMdContent(mdHeader + mdContent);
+
+        Assert.assertEquals(
+                mdContent,
+                this.realPage.getMdContent()
+        );
+    }
+
+}

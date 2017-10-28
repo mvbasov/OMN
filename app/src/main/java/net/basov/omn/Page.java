mvbasov/OMN

commit bd05422aec90bd0f75c224378b8bcb768a844956

Fix page header parser

app/src/main/java/net/basov/omn/Page.java
@@ -79,6 +79,7 @@ public class Page {
             if (inHeader) {
                 if (trim.length() == 0) {
                     inHeader = false;
+                    sb.append("\n");
                     continue;
                 }
                 String[] filds = trim.split(":", 2);
@@ -157,7 +158,7 @@ public class Page {
                 sb.append(str).append("\n");
             }
         }
-        if(!emptyHeader) sb.append(" \n");
+        //if(!emptyHeader) sb.append(" \n");
         this.mMdContent = sb.toString();
         this.mHeaderMeta = mdHeader;
     }

commit ba3dab63cf18fc3f46e1c9d3d8093a110981a93f

Temporary fix endless loop at new page creation
This bug introduced by commit bd05422ae

app/src/main/java/net/basov/omn/Page.java
@@ -98,7 +98,8 @@ public class Page {
             if (inHeader) {
                 if (trim.length() == 0) {
                     inHeader = false;
-                    sb.append("\n");
+                    // TODO: *** (part 1 of 2)the following string is workarround to fix endless loop at new page creation
+                    //sb.append("\n");
                     continue;
                 }
                 String[] filds = trim.split(":", 2);
@@ -177,7 +178,8 @@ public class Page {
                 sb.append(str).append("\n");
             }
         }
-        //if(!emptyHeader) sb.append(" \n");
+        // TODO: *** (part 2 of 2)the following string is workarround to fix endless loop at new page creation
+        if(!emptyHeader) sb.append(" \n");
         this.mMdContent = sb.toString();
         this.mHeaderMeta = mdHeader;
     }
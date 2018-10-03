c29334244b9a193ec987846c5b281781035d3595
Mikhail Basov (NeedleFile) <omn[at]basov[dot]net>
2018-10-03 18:06:02 +0300

    Add script to create assets archive
    Create assetc archive for mobile development with AIDE

---
+++ pack_assets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GCOMMIT=`git rev-parse --short HEAD`
+GLTAG=`git  describe --tags --abbrev=0`
+GCOUNT=`git rev-list --count ${GLTAG}..HEAD`
+
+cd app/src/main/
+tar -zcvf ../../../OMN_assets.${GLTAG}-${GCOUNT}-${GCOMMIT}.tgz assets/js/highlight.pack.js assets/css/highlight.css assets/js/marked.min.js assets/fonts/google-material/MaterialIcons-Regular.woff2
+

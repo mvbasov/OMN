e3477189379bd2d356e9f4114a7d5193c4f4c1da
Mikhail Basov (XMi5s) <mikhail[at]basov[dot]net>
2019-05-24 02:39:04 +0300

    Make page header fixed
    Operate but 1-st header toggle add extra space
    on top of the page body. The next toggles normal.
    Need to fix this issue.

--- app/src/main/assets/js/functions.js
+++ app/src/main/assets/js/functions.js
@@ -1,17 +1,23 @@
+function setBodyTop(){
+  var sticky = document.getElementById('ptitle').offsetHeight;
+  document.getElementById('content').style.paddingTop=sticky+'px'; 
+}
+
 /*
  * Function to show/hide page header
 */
 
 function toggleHdrBtnVis() {
-	hheader=document.getElementById('hidable_header');
-    btn=document.getElementById('title_arrow');
-    if (hheader.style.display !== 'none') {
-        hheader.style.display = 'none';
-        btn.innerHTML = '&nbsp;+&nbsp;';
-    } else {
-        hheader.style.display = 'block';
-        btn.innerHTML = '&nbsp;&#x2013;&nbsp;';
-    }
+  hheader=document.getElementById('hidable_header');
+  btn=document.getElementById('title_arrow');
+  if (hheader.style.display !== 'none') {
+    hheader.style.display = 'none';
+    btn.innerHTML = '&nbsp;+&nbsp;';
+  } else {
+    hheader.style.display = 'block';
+    btn.innerHTML = '&nbsp;&#x2013;&nbsp;';
+  }
+  setBodyTop();
 }
 
 function findHeadings(root, sects){

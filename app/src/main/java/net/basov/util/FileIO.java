421b64a0bcd1c2288673033285ca3258d8fbfd48
Mikhail Basov (NeedleFile) <omn[at]basov[dot]net>
2023-01-27 22:25:51 +0300

    Force to overwrite Bookmarker JS and CSS on update

--- app/src/main/java/net/basov/util/FileIO.java
+++ app/src/main/java/net/basov/util/FileIO.java
@@ -353,16 +353,16 @@ public class FileIO {
                 Constants.HIGHLIGHT_CSS,
                 Constants.ICONS_FONT,
                 Constants.ICONS_CSS,
-                Constants.FUNCTIONS_JS
+                Constants.FUNCTIONS_JS,
+                Constants.URL_INCOMING_CSS,
+                Constants.URL_INCOMING_JS
         };
         copyFilesFromAssets(c, files, force);
 
         String[] optionalFiles = {
                 Constants.CUSTOM_CSS,
                 Constants.GITIGNORE,
-                "md/" + Constants.URL_INCOMING_PAGE + ".md",
-                Constants.URL_INCOMING_CSS,
-                Constants.URL_INCOMING_JS
+                "md/" + Constants.URL_INCOMING_PAGE + ".md"
         };
         copyFilesFromAssets(c, optionalFiles, false);
 

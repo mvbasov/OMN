befb7891c6fca540bbc41f1040467ddb1131672d
Mikhail Basov (MM6) <mikhail[at]basov[dot]net>
2024-08-22 14:38:52 +0300

    Update (c) date to 2024

--- app/src/main/java/net/basov/omn/NoteProvider.java
+++ app/src/main/java/net/basov/omn/NoteProvider.java
@@ -2,7 +2,7 @@ package net.basov.omn;
 /*
 Open Markdown Notes (android application to take and organize everyday notes)
 
-Copyright (c) 2017-2023 Mikhail Basov (https://github.com/mvbasov/OMN)
+Copyright (c) 2017-2024 Mikhail Basov (https://github.com/mvbasov/OMN)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
